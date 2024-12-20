7616310a8346c41537b972b29f281830c9675bf9
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2024-12-20 15:37:18 +0000

    Arrays

---
+++ Collections Working with JavaScript Arrays/index.js
@@ -0,0 +1,9 @@
+var guestList = ["Angela", "Joel", "Jack", "Mods", "Soren"];
+
+var person = "Soren";
+
+if (guestList.includes(person)) {
+  console.log(person + " Is on the list!");
+} else {
+  console.log(person + " Is not on the list!");
+}
