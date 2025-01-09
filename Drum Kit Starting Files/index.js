9c4583ca720d6721b436c31c8a6a07a4dfffee99
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2025-01-09 21:33:16 +0000

    Drum Kit update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,6 +4,12 @@ for (var i = 0; i < numberOfButtons; i++) {
   document
     .querySelectorAll("button.drum")
     [i].addEventListener("click", function () {
-      alert("clicked");
+      this.style.color = "white";
     });
 }
+
+/* 
+var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+
+*/
