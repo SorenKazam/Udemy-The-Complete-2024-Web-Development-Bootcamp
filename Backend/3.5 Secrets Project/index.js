155d1a51c8f1eea0422d3e00a4138c07979d57df
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2025-02-01 16:20:38 +0000

    Secrets Project

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,42 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+//The password is
+import express from "express"; /* node server */
+import bodyParser from "body-parser"; /* handle forms */
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var password = "";
+var correctPassword = "ILoveProgramming";
+var userIsAuth = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function verifyPassword(req, res, next) {
+  password = req.body["password"];
+  if (password === correctPassword) {
+    userIsAuth = true;
+  }
+  next();
+}
+
+app.use(verifyPassword);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuth) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    console.log("Incorrect password");
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
