5d5f60556e6cbedfc799a2616127193093d9ee1b
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2025-02-01 15:34:14 +0000

    Middlewares

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNameGenerator(req, res, next) {
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is: ${bandName}</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
