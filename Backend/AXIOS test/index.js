48cc595080465dedecb9ea153826f6676a6657a9
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2025-02-19 21:17:44 +0000

    5.3 API no longer working

--- Backend/AXIOS test/index.js
+++ Backend/AXIOS test/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
 axios
-  .get("https://jsonplaceholder.typicode.com/users")
+  .get(API_URL)
   .then((response) => {
     console.log(response.data); // Exibe os dados dos usuários
   })
@@ -11,13 +13,17 @@ axios
 
 async function getUsers() {
   try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
+    const response = await axios.get(API_URL);
     console.log(response.data); // Aqui temos os dados da API
+    return response.data;
   } catch (error) {
     console.error("Erro na requisição:", error);
+    throw error;
   }
 }
 
-getUsers(); // Chamamos a função para buscar os dados
+getUsers().then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.error(error);
+});
